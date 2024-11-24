ee4a98b007a6b56ccde7c16c5d94734bc749f001
Sofia Skakun <sofiaskakun30@gmail.com>
2024-11-24 16:08:10 +0300

    idex.py

--- idex.py
+++ idex.py
@@ -1,84 +1,93 @@
-import json
+import json 
 
-def failik():
+def file():
     with open("dump.json", "r", encoding="utf-8") as file:
         return json.load(file)
 
 def menu():
     print("-----------------Меню-----------------")
     print("-------1. Вывести все записи----------")
-    print("-------2. Вывести запись по полю----------")
+    print("-------2. Вывести запись по ID----------")
     print("-------3. Добавить запись----------")
-    print("-------4. Удалить запись по полю----------")
+    print("-------4. Удалить запись по ID----------")
     print("-------5. Выйти из программы----------")
+
+def correct_input(prompt):
+    while True:
+        try:
+            option = int(input(prompt))
+            if option <= 0 or option > 5:
+                print("Введите корректный номер.")
+            else:
+                return option
+        except ValueError:
+            print("Введите число.")
 
 def first(data):
     print("Все записи:")
-    for d in data:
-        print(json.dumps(d))
+    for entry in data:
+        print(json.dumps(entry))
 
 def second(data):
-    idd = int(input("Введите id записи для поиска: "))
-    find = False
-    for d in data:
-        if d["id"] == idd:
-            print(json.dumps(d, indent=4))
-            find = True
+    search_id = int(input("Введите ID записи для поиска: "))
+    found = False
+    for entry in data:
+        if entry["id"] == search_id:
+            print(json.dumps(entry, indent=4))
+            found = True
             break
-    if not find:
+    if not found:
         print("Запись не найдена.")
 
 def third(data):
-    new = {
-        "id": int(input("Введите id: ")),
+    new_record = {
+        "id": int(input("Введите ID: ")),
         "name": input("Введите общее название рыбы: "),
         "latin_name": input("Введите латинское название рыбы: "),
-        "is_salt_water_fish": str(input("Является ли рыба соленоводной (yes/no)? ")),
+        "is_salt_water_fish": input("Является ли рыба соленоводной? "),
         "sub_type_count": int(input("Введите количество подвидов: "))
     }
-    data.append(new)
+    data.append(new_record)
     with open("dump.json", 'w', encoding='utf-8') as file:
-        json.dump(data, file, indent=4)
+        json.dump(data, file, indent=4, ensure_ascii=False)
     return 1
 
 def fourth(data):
-    iddd = int(input("Введите id: "))
-    find = False
-    for index, d in enumerate(data):
-        if d["id"] == iddd:
+    delete_id = int(input("Введите ID для удаления: "))
+    found = False
+    for index, entry in enumerate(data):
+        if entry["id"] == delete_id:
             del data[index]
-            find = True
+            found = True
             break
-    if not find:
+    if not found:
         print("Запись не найдена.")
 
 def main():
-    s = failik()
-    C = 0
+    data = file()
+    operation_count = 0
 
     while True:
         menu()
-        a = int(input("Введите номер пункта: "))
+        option = correct_input("Введите номер пункта: ")
 
-        if a <= 0 or a > 5:
-            print("Введите корректный номер.")
-            continue
-
-        elif a == 1:
-            first(s)
-            C += len(s)
-
-        elif a == 2:
-            second(s)
-        
-        elif a == 3:
-            C += third(s)
-
-        elif a == 4:
-            fourth(s)
-
-        elif a == 5:
-            print(f"Количество выполненных операций с записями: {C}")
+        if option == 1:
+            first(data)
+            operation_count += 1
+            
+        elif option == 2:
+            second(data)
+            operation_count += 1
+            
+        elif option == 3:
+            operation_count += third(data)
+            
+        elif option == 4:
+            fourth(data)
+            operation_count += 1
+            
+        elif option == 5:
+            print(f"Количество выполненных операций с записями: {operation_count}")
             break
 
 if __name__ == "__main__":
